3afe26b2dfc0b65662693619ecea70ac9c02a2d2
Toshukojo <Toshukojo@users.noreply.github.com>
2020-08-19 22:18:21 -0400

    etl_pipeline csv to sqlite3

---
+++ etl_pipleine.py
@@ -0,0 +1,16 @@
+
+import csv
+import sqlite3
+import pandas as pd
+"""Pipeline"""
+con = sqlite3.connect('songs.db')
+cur = con.cursor()
+cur.execute("""CREATE TABLE song (acousticness NUMERIC, artists TEXT, 
+            danceability NUMERIC, duration_ms NUMERIC, energy NUMERIC,explicit NUMERIC,
+            id TEXT,instrumentalness NUMERIC, key INTEGER,liveness NUMERIC,
+            loudness NUMERIC,mode INTEGER,name TEXT,popularity NUMERIC, release_date TEXT,
+            speechiness NUMERIC,tempo NUMERIC,valence NUMERIC,year TEXT);""")
+
+
+df = pd.read_csv('data.csv')
+df.to_sql('song', con, if_exists='append', index=False)
