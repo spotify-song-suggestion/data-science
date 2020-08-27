f49d50861f31214b55f9ebf333bd6457df1dc1e6
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-08-27 16:32:19 -0400

    refactoring

--- beats/app.py
+++ beats/app.py
@@ -29,8 +29,8 @@ def create_app():
     app = Flask(__name__)
 
     # TODO switch to PostgreDB
-    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-    # app.config["SQLALCHEMY_DATABASE_URI"] = getenv("SQLITE3_URL")
+    #app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("SQLITE3_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     
