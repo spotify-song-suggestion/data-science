5c5f170aa2c30bed3224572d45ddad30bcccd78d
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-08-27 11:10:54 -0400

    fix slashes

--- beats/suggest.py
+++ beats/suggest.py
@@ -2,10 +2,12 @@ import pickle
 import pandas as pd
 
 
-
-filename = 'beats\\testing_model.sav'
+# TODO check slashes window and mac
+# filename = 'beats\\testing_model.sav'
+filename = 'beats/testing_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
-songs_new = pd.read_pickle("beats\\ML_model_db.pkl")
+# songs_new = pd.read_pickle("beats\\ML_model_db.pkl")
+songs_new = pd.read_pickle("beats/ML_model_db.pkl")
 
 
 def find_recommended_songs(audio_features):
